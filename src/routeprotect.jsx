f5c7593b7797fae5854ab153579cff5b430a3fc1
Viz2202 <vishnurejeesh22@gmail.com>
2025-11-16 23:38:11 +0530

    testing

--- src/routeprotect.jsx
+++ src/routeprotect.jsx
@@ -2,6 +2,8 @@ import { Navigate } from "react-router-dom";
 
 function RouteProtect({ children }) {
   const isLoggedIn = sessionStorage.getItem("isLoggedIn");
+  console.log(isLoggedIn);
+  console.log(typeof(isLoggedIn));
   return isLoggedIn === "true" ? children : <Navigate to="/" replace />;
 }
 
