42ddf044c5e88281ed6b106b5d21b7eed1b374db
Viz2202 <vishnurejeesh22@gmail.com>
2025-11-16 23:55:33 +0530

    Bug Fix

--- src/routeprotect.jsx
+++ src/routeprotect.jsx
@@ -2,9 +2,11 @@ import { Navigate } from "react-router-dom";
 
 function RouteProtect({ children }) {
   const isLoggedIn = sessionStorage.getItem("isLoggedIn");
-  console.log(isLoggedIn);
-  console.log(typeof(isLoggedIn));
-  return isLoggedIn === "true" ? children : <Navigate to="/" replace />;
+  if(!isLoggedIn){
+    sessionStorage.clear();
+    <Navigate to="/" replace />
+  }
+  return children; /*isLoggedIn === "true" ? children : <Navigate to="/" replace />*/
 }
 
 export default RouteProtect;
